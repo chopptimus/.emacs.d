ac31abced5c10707e685cd9a7a338c08c69c2517
Hugo Young <hybas3@gmail.com>
2017-10-15 00:28:32 +0100

    Rework
    - Use dracula theme
    - Use custom.el file for customize variables
    - Use helm

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
+;; General
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+
+;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (setq package-enable-at-startup nil)
 (package-initialize)
 
@@ -11,21 +19,54 @@
   :ensure t
   :init
   (setq evil-want-C-u-scroll t)
+  :config
   (evil-mode 1))
 
-(use-package atom-one-dark-theme
+(use-package helm
+   :ensure t
+   :pin melpa-stable
+   :init
+   (progn
+     (require 'helm-config)
+     (helm-mode))
+   :bind
+   ("M-x" . helm-M-x)
+   ("C-h a" . helm-apropos))
+
+(use-package projectile
+  :ensure t)
+
+(use-package cider
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+(use-package dracula-theme
   :ensure t
   :init
-  (load-theme 'atom-one-dark t))
+  (load-theme 'dracula t))
 
-;; Ease some of the gui pain
+;; Hooks
+(defun add-hook-m (hs fn) (mapc (lambda (h) (add-hook h fn)) hs))
+
+(defun my-prog-mode ()
+  (linum-mode)
+  (lambda () (modify-syntax-entry ?_ "w")))
+
+(defun my-lisp-mode ()
+  (modify-syntax-entry ?- "w")
+  (rainbow-delimiters-mode))
+
+(add-hook-m '(clojure-mode-hook emacs-lisp-mode-hook) 'my-lisp-mode)
+(add-hook 'prog-mode-hook 'my-prog-mode)
+
 (setq ring-bell-function 'ignore)
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+(blink-cursor-mode 0)
 
-(set-default-font "Source Code Pro-15")
-
-;; General
-(global-linum-mode)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
