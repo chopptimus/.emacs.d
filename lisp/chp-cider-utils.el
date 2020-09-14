b8c2a4f11d5288174c1a4ebb9088c9d611ba4bbf
Hugo Young <hybas3@gmail.com>
2020-09-14 01:29:50 +0100

    Remove pointless cider dependency

--- lisp/chp-cider-utils.el
+++ lisp/chp-cider-utils.el
@@ -4,38 +4,29 @@
 ;;; Utility functions for cider.
 
 ;;; Code:
-(require 'cider-client)
+(require 'parseedn)
 
 (defun chp-cider-def-args ()
   "Insert a form defining all the args for debugging.
 Doesn't work for multiple arity functions."
   (interactive)
   (save-excursion
-    (forward-char)
-    (beginning-of-defun)
-    (forward-char)
-    (forward-sexp 2)
-    ; Heuristic for skipping over return type hints
-    (when (< 64 (string-to-char (substring (current-word) 0 1)) 91)
-      (forward-sexp))
-    (let* ((args (nrepl-dict-get
-                  (cider-var-info (current-word))
-                  "arglists-str"))
-           (syms (seq-remove
-                  (lambda (s)
-                    (member (string-to-char (substring s 0 1)) '(?: ?^)))
-                  (split-string
-                   (apply #'string
-                          (seq-remove
-                           (lambda (c) (memq c '(?\[ ?\] ?\{ ?\})))
-                           args))
-                   " " t))))
-      (while (not (= (preceding-char) 93))
+    (let ((form (save-mark-and-excursion
+                  (mark-defun)
+                  (parseedn-read-str (buffer-substring-no-properties
+                                      (region-beginning)
+                                      (region-end))))))
+      (beginning-of-defun)
+      (forward-char)
+      (while (not (vectorp (car form)))
+        (setq form (cdr form))
         (forward-sexp))
-      (dolist (sym syms)
+      (forward-sexp)
+      (seq-doseq (arg (car form))
         (newline)
         (indent-for-tab-command)
-        (insert "(def " sym " " sym ")"))
+        (let ((s (symbol-name arg)))
+          (insert "(def " s " " s ")")))
       (indent-for-tab-command))))
 
 (provide 'chp-cider-utils)
