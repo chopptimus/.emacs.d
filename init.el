04cf91285482821ffca66a0c84c11cae9490dfcd
Hugo Young <hybas3@gmail.com>
2021-01-11 23:50:28 +0000

    Rust

--- init.el
+++ init.el
@@ -313,5 +313,14 @@ checkers"
   :config
   (exec-path-from-shell-initialize))
 
+(use-package rust-mode
+  :ensure t)
+
+(use-package flycheck-rust
+  :ensure t
+  :init
+  (with-eval-after-load 'rust-mode
+   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+
 (provide 'init)
 ;;; init.el ends here
